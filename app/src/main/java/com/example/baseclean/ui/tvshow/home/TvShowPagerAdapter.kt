82f45008de172cb9bd81f496a63329b1ebbf3a20
David Lara <david.lara@globant.com>
2021-06-11 11:22:27 -0500

    feat: home tabs

--- app/src/main/java/com/example/baseclean/ui/tvshow/home/TvShowPagerAdapter.kt
+++ app/src/main/java/com/example/baseclean/ui/tvshow/home/TvShowPagerAdapter.kt
@@ -4,9 +4,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class TvShowPagerAdapter (fa: FragmentActivity, private val tvTabs: List<Fragment>) : FragmentStateAdapter(fa) {
+const val NUMBER_OF_PAGES = 3
+
+class TvShowPagerAdapter(fa: FragmentActivity, private val tvTabs: List<Fragment>) :
+    FragmentStateAdapter(fa) {
     override fun getItemCount(): Int {
-        return 3
+        return NUMBER_OF_PAGES
     }
 
     override fun createFragment(position: Int): Fragment {
